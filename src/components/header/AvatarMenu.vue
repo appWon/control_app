664c76642aec12971fc1062377d913e2a2630d1c
appWon <korkrip11@gmail.com>
2021-03-04 07:16:58 +0900

    수정사항 1. navbar 구현 ㄴ추후 로그아웃 기능 구현

---
+++ src/components/header/AvatarMenu.vue
@@ -0,0 +1,54 @@
+<template>
+  <v-menu
+    bottom
+    min-width="200px"
+    rounded
+    offset-y
+  >
+    <template v-slot:activator="{ on }">
+      <v-btn
+        icon
+        x-large
+        v-on="on"
+      >
+        <v-avatar size="40">
+          <img src="https://avatars.githubusercontent.com/u/46001535?s=460&u=08a08f87c7b68c83cdc298cc6263b82cffd9c360&v=4">
+        </v-avatar>
+      </v-btn>
+    </template>
+    <v-card>
+      <v-list-item-content class="justify-center">
+        <div class="mx-auto text-center">
+          <v-avatar>
+            <img src="https://avatars.githubusercontent.com/u/46001535?s=460&u=08a08f87c7b68c83cdc298cc6263b82cffd9c360&v=4">
+          </v-avatar>
+          <h3 class="my-2">{{ user.name }}</h3>
+          <p class="caption mt-1">
+            {{ user.email }}
+          </p>
+          <v-divider class="my-3"></v-divider>
+          <v-btn
+            depressed
+            rounded
+            text
+          >
+            로그아웃
+          </v-btn>
+        </div>
+      </v-list-item-content>
+    </v-card>
+  </v-menu>
+</template>
+
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component
+export default class AvatarMenu extends Vue {
+  private user = {
+    name : "장재원",
+    email : "korkrip11@gmail.com"
+  };
+}
+</script>
---
+++ src/components/header/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-card color="grey lighten-4">
+    <v-toolbar>
+      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-spacer></v-spacer>
+      <AvartarMenu />
+    </v-toolbar>
+  </v-card>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+import AvartarMenu from "./AvatarMenu.vue";
+
+@Component({
+  components : {
+    AvartarMenu
+  }
+})
+
+export default class Header extends Vue {
+  private isMenuToggle  = false;
+}
+</script>
